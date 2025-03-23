ac014344cee285ebbc2fbf28b19775e6980ff5b3
Thermal <74275853+ThermaloS@users.noreply.github.com>
2025-03-22 22:14:08 -0500

    Delete bin/cogs/music/radio directory

--- bin/cogs/music/radio/__init__.py
+++
@@ -1,7 +0,0 @@
-# Radio package initialization
-# This file makes the radio directory a Python package
-
-from bin.cogs.music.radio.radio_cog import RadioCog
-
-# Export the main RadioCog class
-__all__ = ['RadioCog']
